2b03ca47adde6379d312b8a83e5d027f8978446a
Filipe Veloso <filipe.roman.veloso@gmail.com>
2016-01-12 21:50:51 -0200

    custom 1%

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
 # create default test user
-User.create!(name: "Chatty Bot", email: "chattybot@example.com", password: "12345", password_confirmation: "12345")
+
+if Rails.env.development? || Rails.env.staging?
+  20.times do |n|
+    User.create!(name: "R2D#{n}", email: "chattybot+#{n}@example.com", password: "12345", password_confirmation: "12345")
+  end
+end
